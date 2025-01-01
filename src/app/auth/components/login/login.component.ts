8c69ea3c54401cd3316f34492d4bdd3b53958288
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2025-01-02 00:07:02 +0530

    Created Storage Service to store JWT and User and fixed some bugs

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../register/services/auth/auth.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
+import { UserStorageService } from '../../services/storage/user-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -28,6 +29,15 @@ export class LoginComponent {
   submitForm(){
     this.authService.login(this.loginForm.value).subscribe(res=>{
       console.log(res);
+      if(res.userId != null){
+        const user = {
+          id: res.userId,
+          role: res.userRole
+        }
+
+        UserStorageService.saveUser(user);
+        UserStorageService.saveToken(res.jwt);
+      }
     }, error=>{
       this.message
       .error(
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from './services/auth/auth.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
