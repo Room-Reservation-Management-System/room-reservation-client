3de3f6a83776e05461ffa19c8fc9da664b499133
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2025-01-03 23:18:11 +0530

    Created modules for Admin and Customer

--- src/app/modules/customer/customer.module.ts
+++ src/app/modules/customer/customer.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    RoomsComponent
   ],
   imports: [
     CommonModule,
