03230140d37411eb77bfade6e826999c05dd50ef
Prasanjith76 <prasanjith123.lorensuhewa@gmail.com>
2025-01-05 10:43:17 +0530

    called getRooms API call

--- src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../admin-services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  currentPage = 1;
+  rooms = [];
+
+  constructor(private adminService: AdminService,
+    private message: NzMessageService,
+  ){
+    this.getRooms();  
+  }
+
+  getRooms(){
+    this.adminService.getRooms(this.currentPage -1).subscribe(res=>{
+      console.log(res);
+      this.rooms = res;
+    })
+  }
 }
